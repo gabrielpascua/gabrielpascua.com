9c23c215af9f448569e1d97fabcf720daa2ec4ea
Gabriel Pascua <gabriel.pascua@wennermedia.com>
2016-03-10 19:23:08 -0500

    Creating columns

--- js/resume.js
+++ js/resume.js
@@ -17,7 +17,7 @@
 	ctl.controller('WorkListController', ['$http', 'Work', function($http, Work){
 		
 		this.jobs = Work.query();
-        this.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 		
 	}]);
 	
