0dce21e2c9d7210908b591bdd4296f24836bfc97
Gabriel Pascua <brakes2k3@yahoo.com>
2016-06-17 17:50:11 -0400

    Porting resume

---
+++ js/resume/resume.js
@@ -0,0 +1,33 @@
+(function () {
+
+  //services
+  var svc = angular.module('workServices', ['ngResource']);
+  svc.factory('Work', ['$resource', function ($resource) {
+
+    return $resource('/data/resume.json', {}, {
+      query: {method: 'GET', isArray: false}
+    });
+
+  }]);
+
+
+  //controllers
+  var ctl = angular.module('workControllers', []);
+  ctl.controller('WorkListController', ['$http', 'Work', function ($http, Work) {
+
+    this.jobs = Work.query();
+    this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  }]);
+
+
+  //app
+  var app = angular
+    .module('resume', ['workControllers', 'workServices'])
+    .config(function($interpolateProvider) {
+      //change curly braces b/c of jekyll conflict
+      $interpolateProvider.startSymbol('//');
+      $interpolateProvider.endSymbol('//');
+    });
+
+})();
