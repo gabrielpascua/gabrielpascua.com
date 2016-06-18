1312998880fd3e73469ee8b00f395101277b4f20
Gabriel Pascua <brakes2k3@yahoo.com>
2016-06-18 07:20:05 -0400

    Moving onload function

--- js/resume/resume.js
+++ js/resume/resume.js
@@ -1,13 +1,29 @@
 (function () {
 
+  //private functions
+  function getTheme(){
+    var themes = ['default', 'kobe-htm','manhattan', 'sharpie'];
+    var random;
+
+    var qsTemplate = window.location.search.substr(1).split('&')
+      .filter(function(part){
+        return part.indexOf('template=') >= 0;
+      });
+
+    if(qsTemplate.length > 0){
+      return qsTemplate[0].split('=')[1];
+    }else{
+      random = Math.floor(Math.random() * themes.length) + 1;
+      return themes[random-1];
+    }
+  }
+
   //services
   var svc = angular.module('workServices', ['ngResource']);
   svc.factory('Work', ['$resource', function ($resource) {
-
     return $resource('/data/resume.json', {}, {
       query: {method: 'GET', isArray: false}
     });
-
   }]);
 
 
@@ -16,6 +32,7 @@
   ctl.controller('WorkListController', ['$http', 'Work', function ($http, Work) {
 
     this.jobs = Work.query();
+    this.theme = getTheme();
     this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
   }]);
