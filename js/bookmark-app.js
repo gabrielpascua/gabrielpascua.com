35aac887fbfc25f873e0f0fc75bde4e7f4d5878d
Gabriel Pascua <brakes2k3@yahoo.com>
2016-07-09 18:48:59 -0400

    Wiring the autocomplete search

--- js/bookmark-app.js
+++ js/bookmark-app.js
@@ -9,6 +9,112 @@ function sortAscending(item1,item2){
   }
 }
 
+function AutoCompleteItem(){
+  this.href = '';
+  this.text = '';
+  this.group = '';
+}
+
+var AutoCompleteResult = React.createClass({
+  render: function(autocompleteItem){
+    if(!autocompleteItem){
+      return (<a href="#">No matching bookmark found.</a>);
+    }else{
+      return (<a href={autocompleteItem.href}><b>{autocompleteItem.group}</b> - {autocompleteItem.text}</a>);
+    }
+  }
+});
+
+/* Parent - Child callback interaction */
+// 1. LinkSearchBox loads default term from LinkSearch state 
+// 2. LinkSearcher assigns searchForLinksCallback function as a property to LinkSearchBox
+// 3. From LinkSearchbox onChange is triggered when users type something and executes the callback
+// 4. callback function from LinkSearcher gets the value from LinkSearchBox and set it on it's own state
+// 5. LinkSearchbox text value gets updated as user types
+
+var LinkSearchBox = React.createClass({
+  // # 3
+  setSearchTerm: function(event){
+    this.props.onSearch(event.target.value);
+  },
+  render: function(){
+    return(
+      <input type="text" placeholder="Search Bookmarks"
+        value={this.props.term}        
+        onChange={this.setSearchTerm} 
+      />
+    );
+  }
+});
+
+var LinkSearcher = React.createClass({
+  getInitialState: function(){
+    return {
+      searchTerm: ''
+    };
+  },
+  ///# 4
+  searchForLinksCallback: function(searchboxValue){
+    this.setState({
+      searchTerm: searchboxValue
+    });
+  },
+  render: function(){
+    return(
+      <div className="bookmark-search">
+        {/* #1 */}                                           {/* #2 */}
+        <LinkSearchBox term={this.state.searchTerm} onSearch={this.searchForLinksCallback}/>
+        <div className="bookmark-results">
+          <LinkSearchResults filter={this.state.searchTerm} data={this.props.data} />
+        </div>
+      </div>
+    );
+  }
+});
+
+var LinkSearchResults = React.createClass({
+  getInitialState: function(){
+    return { results: [] };
+  },
+  getDefaultProps: function(){
+    return { data: [] };
+  },
+  searchData: function(filter){
+    var regex = new RegExp(filter, 'ig');
+    var result = [];
+    
+    this.props.data.forEach(function(group){
+      group.links.forEach(function(link){
+        if(regex.test(link.name) || regex.test(link.url)){
+          result.push({
+            group: group.name,
+            text: link.name,
+            href: link.url
+          });
+        };
+      });
+    });
+    
+    return result;
+  },
+  render: function(){
+    var results = this.props.filter.length ? this.searchData(this.props.filter) : [];    
+    return(
+      <ul>
+      {
+        results.map(function(link, linkIndex){
+          return( 
+            <li key={linkIndex}>
+                <a href={link.href}><b>link.group</b> - {link.text}</a>
+            </li>
+          )
+        })  
+      }
+      </ul>
+    );
+  }
+});
+
 var LinkGroup = React.createClass({
   render: function(){
     var linkLists = this.props.data
@@ -102,84 +208,87 @@ var BookmarkApp = React.createClass({
         // });
 
         /* autocomplete */
-        function AutoCompleteResult(anchorTag){
-          this.href = '';
-          this.text = '';
-          this.group = '';
+        // function AutoCompleteResult(anchorTag){
+        //   this.href = '';
+        //   this.text = '';
+        //   this.group = '';
 
-          if(anchorTag){
-            this.href = anchorTag.attr('href');
-            this.text = anchorTag.text();
-          }
+        //   if(anchorTag){
+        //     this.href = anchorTag.attr('href');
+        //     this.text = anchorTag.text();
+        //   }
 
-          this.toHTML = function(){
-            if(this.href === ''){
-              return '<a href="#">No matching bookmark found.</a>';
-            }else{
-              return '<a href="' + this.href + '"><b>' + this.group + '</b> - ' + this.text + '</a>';
-            }
-          }
-        }
+        //   this.toHTML = function(){
+        //     if(this.href === ''){
+        //       return '<a href="#">No matching bookmark found.</a>';
+        //     }else{
+        //       return '<a href="' + this.href + '"><b>' + this.group + '</b> - ' + this.text + '</a>';
+        //     }
+        //   }
+        // }
 
-        var dataset = [];
-        $('.bookmark-row').each(function(){
-          var row = $(this);
-          var group = row.find('h3 a:first-child').text();
-          row.find('li').each(function(){
-            var li = $(this);
-            li.find('a').each(function(){
-              var ac = new AutoCompleteResult($(this));
-              ac.group = group;
-              dataset.push(ac);
-            });
-          });
-        });
+        // var dataset = [];
+        // $('.bookmark-row').each(function(){
+        //   var row = $(this);
+        //   var group = row.find('h3 a:first-child').text();
+        //   row.find('li').each(function(){
+        //     var li = $(this);
+        //     li.find('a').each(function(){
+        //       var ac = new AutoCompleteResult($(this));
+        //       ac.group = group;
+        //       dataset.push(ac);
+        //     });
+        //   });
+        // });
 
-        var autocomplete = $('.bookmark-search input[type="text"]').focus();
-        var bookmarkResults = $('.bookmark-results');
+        // var autocomplete = $('.bookmark-search input[type="text"]').focus();
+        // var bookmarkResults = $('.bookmark-results');
 
-        function positionResults(e){
-          if(e.keyCode === 27){
-            bookmarkResults.html('').removeAttr('style');
-          }else{
-            bookmarkResults.css({
-              display: 'block',
-              left: autocomplete.position().left + 'px',
-              width: autocomplete.outerWidth() + 'px'
-            });
-          }
-        };
+        // function positionResults(e){
+        //   if(e.keyCode === 27){
+        //     bookmarkResults.html('').removeAttr('style');
+        //   }else{
+        //     bookmarkResults.css({
+        //       display: 'block',
+        //       left: autocomplete.position().left + 'px',
+        //       width: autocomplete.outerWidth() + 'px'
+        //     });
+        //   }
+        // };
 
-        autocomplete.on('keyup', function(e){
-          var txt = $(this);
-          var regex;
-          var matches = dataset.filter(function(data){
-            regex = new RegExp(txt.val(), 'ig');
-            return regex.test(data.text) || regex.test(data.href);
-          });
-          if(!matches.length){
-            matches.push(new AutoCompleteResult());
-          }
+        // autocomplete.on('keyup', function(e){
+        //   var txt = $(this);
+        //   var regex;
+        //   var matches = dataset.filter(function(data){
+        //     regex = new RegExp(txt.val(), 'ig');
+        //     return regex.test(data.text) || regex.test(data.href);
+        //   });
+        //   if(!matches.length){
+        //     matches.push(new AutoCompleteResult());
+        //   }
 
-          var results = '<ul>';
-          for(var i=0; i<matches.length; i++){
-            results += '<li>' + matches[i].toHTML() + '</li>';
-          }
-          results += '</ul>';
+        //   var results = '<ul>';
+        //   for(var i=0; i<matches.length; i++){
+        //     results += '<li>' + matches[i].toHTML() + '</li>';
+        //   }
+        //   results += '</ul>';
 
-          bookmarkResults.html(results);
-          positionResults(e);
-        });
+        //   bookmarkResults.html(results);
+        //   positionResults(e);
+        // });
 
-        $(document).on('keyup', function(e){
-          positionResults(e);
-        });
+        // $(document).on('keyup', function(e){
+        //   positionResults(e);
+        // });
 
-        window.onresize = positionResults;
+        // window.onresize = positionResults;
   },
   render: function(){
     return (
-      <LinkGroup data={this.state.data} />
+      <div>
+        <LinkSearcher data={this.state.data}/ >
+        <LinkGroup data={this.state.data} />
+      </div>
     );
   }
 });
