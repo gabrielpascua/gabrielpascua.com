2a26272bd9064813419d7dc140ff82ea8f4f78d3
Gabriel Pascua <io@gabrielpascua.com>
2017-01-01 12:27:32 -0500

    date inflection

--- js/calendar/app.js
+++ js/calendar/app.js
@@ -1,4 +1,4 @@
-Vue.filter('isHoliday', function (date) {
+function isHoliday(date) {
     //TODO Extract
     var holidays = [
       '1/2/2017',
@@ -21,13 +21,48 @@ Vue.filter('isHoliday', function (date) {
     }
 
     return '';
-});
+}
+
+function getTense(date) {
+    var now = new Date();
+    var today = new Date((now.getMonth()+1) + '/' + now.getDate() + '/' + now.getFullYear());
+
+    if(!date || !date.getTime){
+      return '';
+    }
+
+    if(date.getTime() < today.getTime()){
+      return 'past';
+    }
+
+    if(date.getTime() === today.getTime()){
+      return 'present';
+    }
+
+    return 'future';
+}
 
 Vue.filter('date', function (date) {
   if(date && date.getDate){
     return date.getDate();
   }
   return date;
+});
+
+Vue.filter('inflect', function (date) {
+  var properties = [];
+
+  if(window.location.search.indexOf('?format=plain') < 0){
+    properties.push(isHoliday(date));
+    properties.push(getTense(date));
+  }
+
+  return properties.reduce(function(classes, property){
+    if(property.length){
+      classes += property + ' ';
+    }
+    return classes;
+  }, '').trim();
 });
 
 new Vue({
