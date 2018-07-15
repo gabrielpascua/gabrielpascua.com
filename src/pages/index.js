f8a874098ceb444a83193b7fc3c63a374071768e
Gabriel Pascua <io@gabrielpascua.com>
2018-07-15 09:51:34 -0400

    switching resume data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,87 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
 import graphql from 'graphql';
-import {TopNav} from '../components/top-nav';
 
-export default ({data}) => (
-  <div>
+export default class HomePage extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const data = this.props.data.allHistoryJson.edges.pop().node;
+    const {bio, history} = data;
+    return (
+      <div>
+        <MetaData title="Gabriel Pascua" />
+        <h1>{bio.first_name} {bio.last_name}</h1>
+        <h3>Work History</h3>
+        <ul>
+          {
+            history.map((w, index) => {
+              return(
+                <li key={index}>
+                  {w.company.name}- {w.company.address} <br />
+                  {w.position}<br />
+                  {w.description}
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+
+}
+
+const MetaData = function(props){
+  return (
     <Helmet>
-      <title>Test</title>
+      <title>{props.title}</title>
     </Helmet>
-    {/* Remove The TopNav */}
-    <TopNav />
-    <h1>Gabriel Pascua</h1>
-    <h3>Work History</h3>
-    <ul>
-      {
-        data.allDataJson.edges[0].node
-          .work.map((w, index) => {
-            return <li key={index}>{w.company}</li>;
-          })
-      }
-    </ul>
-  </div>
-);
+  );
+};
 
-export const query = graphql`
+export const workHistoryQuery = graphql`
   query ResumeQuery {
-    allDataJson
-    {
-      edges{
+    allHistoryJson {
+      edges {
         node {
-          work {
-            company
-            startDate
-            endDate
-            position
-            summary
-            website
+          bio {
+            first_name
+            last_name
+            email
+            phone
+          },
+          history {
+            company {
+              name
+              address
+              city
+              state
+              zip
+              country
+            },
+            position,
+            description,
+            start_date {
+              month
+              day
+              year
+            },
+            javascript,
+            client_side,
+            microsoft,
+            version_control,
+            devops,
+            end_date {
+              month
+              day
+              year
+            },
+            php,
+            databases,        
           }
         }
       }
