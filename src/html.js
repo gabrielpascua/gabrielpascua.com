db3f7d56cb3db6d4286a9ad287ebc0ce4a1a5a21
Gabriel Pascua <gpascua@bned.com>
2018-10-06 13:22:16 -0400

    prettier build integration

--- src/html.js
+++ src/html.js
@@ -8,7 +8,10 @@ export default class HTML extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
