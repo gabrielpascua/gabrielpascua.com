d234b1c3f861e462457a9c8b99e91af9cdf6224b
Gabriel Pascua <io@gabrielpascua.com>
2022-10-24 12:09:26 -0400

    logo animation

--- src/components/top-nav.jsx
+++ src/components/top-nav.jsx
@@ -8,7 +8,16 @@ export default class TopNav extends React.Component {
           <ul>
             <li>
               <a href="/" className="home">
-                gp
+                <div className="text-flip-container">
+                  <span style={{ '--i': 1}}>g</span>
+                  <span style={{ '--i': 2}}>a</span>
+                  <span style={{ '--i': 3}}>b</span>
+                  <span style={{ '--i': 4}}>r</span>
+                  <span style={{ '--i': 5}}>i</span>
+                  <span style={{ '--i': 6}}>e</span>
+                  <span style={{ '--i': 7}}>l</span>
+                  <span style={{ '--i': 8}}>p</span>
+                </div>
               </a>
             </li>
             <li>
