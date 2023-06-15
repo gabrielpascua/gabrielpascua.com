58715d7c562df39769d48dfca19b5f72b045f4d5
Gabriel Pascua <io@gabrielpascua.com>
2023-06-15 12:40:52 -0400

    add proper copyright info

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 export default class FooterNav extends React.Component {
   render() {
+    const year = new Date().getFullYear();
     return (
       <footer className="footer">
         <div className="container">
           <p>
-            <small>&copy; 2021 Gabriel Pascua</small>
+            <small>&copy; {year} Gabriel Pascua</small>
           </p>
         </div>
       </footer>
