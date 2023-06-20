13d495f6b80d4ee7ba0fea3f2abf64453f7e6e03
Gabriel Pascua <io@gabrielpascua.com>
2023-06-20 16:37:46 -0400

    user <article /> for reader view

--- src/templates/posts.jsx
+++ src/templates/posts.jsx
@@ -24,7 +24,7 @@ const PostsTemplate = ({ data }) => {
       </div>
       <div className="page-content">
         <div className="container">
-          <div
+          <article
             className="markdown"
             dangerouslySetInnerHTML={{ __html: post.html }}
           />
